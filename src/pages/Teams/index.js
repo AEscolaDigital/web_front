8be9a90f94b80b34d15911980d06c0bfb3a4c3c3
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-09-21 08:17:51 -0300

    Update

--- src/pages/Teams/index.js
+++ src/pages/Teams/index.js
@@ -1,26 +1,35 @@
 import Nav from "../../components/Nav";
 import Header from "../../components/Header";
-import Input from "../../components/Input";
-import BtnSubmit from "../../components/BtnSubmit";
-import BtnCancel from "../../components/BtnCancel";
-import Modal from "../../components/Modal";
-import { Container } from "./styles";
+import imgMathematics from "../../assets/teams/imgMathematics.svg"
+import iconMore from "../../assets/teams/iconMore.svg"
 
-function Teams(){
-    return(
+import { Container, Section } from "./styles";
+
+function Teams() {
+    return (
         <Container>
+            <Header/>
+            <Nav/>
+            <Section>
+                <div>
+                    <div>
+                        <img src={iconMore} />
+                    </div>
+                    <img src={imgMathematics} />
+                    <div>
+                        <div>Matemática</div>
+                        <div>Prof. Guilherme</div>
+                    </div>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
 
-
-          {/* <Modal title="Criar turma" body={
-              <Input label="Nome completo"/>
-
-          } >
-          </Modal> */}
-          {/* <BtnCancel text="Cancelar" />
-          <BtnSubmit text="Enviar"/> */}
-          <Header/>
-          <Nav/> 
-           
+            </Section>
         </Container>
 
     );
--- src/pages/Teams/styles.js
+++ src/pages/Teams/styles.js
@@ -1,5 +1,27 @@
 import styled from "styled-components";
 
-export const Container = styled.main`
-
+export const Container = styled.div`
+` 
+export const Section = styled.section`
+    width: 1120px;
+    margin-top: 50px;
+    margin-left: calc(50vw - 500px) ;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    column-gap: 25px;
+    row-gap: 30px;
+    >div{
+        width: 260px;
+        height: 250px;
+        
+        background-color: white;
+        border-radius: 20px;
+        >img{
+            display: block;
+            margin: -25px auto 0px auto;
+        }
+        >div:nth-child(1){
+            margin: 10px 0px  0px 215px;
+        }
+    }
 ` 
