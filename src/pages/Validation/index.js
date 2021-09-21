7cf59350b9badeed2f6ad1aea31e6c3ecbea49e6
Gustavo6713 <77388527+Gustavo6713@users.noreply.github.com>
2021-09-21 11:25:35 -0300

    validação

---
+++ src/pages/Validation/index.js
@@ -0,0 +1,37 @@
+import { Container, DivContainer, DivTabela } from "./styled";
+import Header from "../../components/Header";
+import Nav from "../../components/Nav";
+import Input from "../../components/Input";
+
+
+function Validation() {
+    return(
+
+        <Container>
+            <Header/>
+            <Nav/>
+            <div>
+                <DivContainer>
+                    
+                    
+                    <div>
+                       
+                    </div>
+
+                    <DivTabela>
+                        <header>
+                            <div></div>
+                        </header>
+
+                        <div>
+
+                        </div>
+                    </DivTabela>
+                </DivContainer>
+            </div>
+        </Container>
+
+    );
+}
+
+export default Validation;
