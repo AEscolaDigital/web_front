de14de8d739382b8def6a11c39cc62ab81e4efe8
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-09-23 11:22:45 -0300

    update page registerResponsible

--- src/pages/RegisterResponsible/index.js
+++ src/pages/RegisterResponsible/index.js
@@ -1,7 +1,12 @@
 
 
+import BtnCancel from "../../components/BtnCancel";
+import BtnSubmit from "../../components/BtnSubmit";
 import Input from "../../components/Input";
-import { Container, DivHeader, DivRegister } from "./styles";
+import InputFile from "../../components/inputFile";
+import resgisterResponsible from "../../assets/registerResponsible/imageResponsible.svg"
+
+import { Container, DivHeader, DivRegister, DivImage, DivBtn } from "./styles";
 
 function RegisterResponsible() {
     return(
@@ -13,16 +18,37 @@ function RegisterResponsible() {
             </DivHeader>
 
             <DivRegister>
-                <Input label="Nome Completo"/>
-                <Input label="Informe o seu número do RG"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-               
+                <Input label="Nome Completo" width="600px"/>
+                <div>
+                    <Input label="Informe o seu RG" width="300px"/>
+                    <InputFile  />
+                </div>
+              
+                <div>   
+                    <Input label="Informe o seu CPF" width="300px"/>
+                    <InputFile />
+                </div>
+                <Input label="Data de Nascimento" width="300px"/>
+                <Input label="Informe seu CEP" width=" 450px"/>
+                <Input label="Informe a rua onde reside" width="450px"/>
+                
+                <div>
+                    <Input label="Informe o Bairro" width="250px"/>
+                    <Input label="Informe o Número" width="200px"/>
+                </div>
+
+                
+                
+                <DivBtn>
+                    <BtnCancel text="Cancelar" />
+                    <BtnSubmit text="Concluir" />
+                </DivBtn>
+                
             </DivRegister>
+
+            <DivImage>
+                <img src={resgisterResponsible}/>
+            </DivImage>
         </Container>
     );
 }
--- src/pages/RegisterResponsible/styles.js
+++ src/pages/RegisterResponsible/styles.js
@@ -6,33 +6,49 @@ export const Container = styled.div`
 
       display: grid;
       grid-template-columns: 1fr 1fr;
-      grid-template-areas: "header header";
-`
+      grid-template-areas: "header header"  
+                            "DivRegisterResponsible DivImage" ;
 
+`
 export const DivHeader = styled.div`
     max-width: 1300px;
-    grid-area: header;
+    
+    grid-area: header ;
+    
 
     >h1 {
-        
-        color: var(--color-text);
-        font-size: 22px;
-        font-weight: 400;
-        padding-top: 10px;
-        margin: 10px;
-        
-    }
+           color: var(--color-text);
+           font-size: 22px;
+           font-weight: 400;
+           padding: 10px;
+           justify-items: start;
+       }
 `
 
 export const DivRegister = styled.div`
-    grid-area: DivRegister;
-   
-    
-        >div >p{
-            padding: 20px;
-            color: var(--color-text);
-            font-size: 18px;
-            font-weight: bold;
+    grid-area: DivRegisterResponsible;
+       
 
-        }
+       >div {
+          
+       display: grid;
+       grid-template-columns: 1fr 1fr;
+       
+         
+       }
 `
+
+export const DivBtn = styled.div`
+       padding: 20px;
+       margin-top: 25px;
+       
+       `
+
+
+export const DivImage = styled.div`
+       display: flex;
+       align-items: center;
+       justify-content: center;
+       grid-area: DivImage;
+       `
+
