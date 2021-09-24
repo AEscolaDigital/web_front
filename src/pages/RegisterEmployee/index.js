8915ca3163988818d3092802ccc3c1ca9e1794a5
Gustavo <gustavomfernandes6@gmail.com>
2021-09-23 22:16:49 -0300

    atualizado

--- src/pages/RegisterEmployee/index.js
+++ src/pages/RegisterEmployee/index.js
@@ -1,14 +1,29 @@
 import {Container, DivDados, DivImagem} from "./styles";
+import iconRegistroProfessor from "../../assets/registerEmployee/iconRegistroProfessor.svg"
+import Input from "../../components/Input"
 
 function RegisterEmployee() {
     return(
         <Container>
             <DivImagem>
+                <header>
+                    Preencha todos os dados a seguir, para se cadastrar na plataforma.
+                </header>
 
+                <div>
+                    <img src={iconRegistroProfessor} />
+                </div>
             </DivImagem>
 
             <DivDados>
-                
+                <header>
+                    Dados do  Funcionario
+                </header>
+
+                <div>
+                    <Input label="Nome Completo" width="553px"/>
+                    
+                </div>
             </DivDados>
         </Container>
     )
--- src/pages/RegisterEmployee/styles.js
+++ src/pages/RegisterEmployee/styles.js
@@ -1,10 +1,45 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    max-width: 1440px;
+    margin: auto;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    //display: flex;
+    //justify-content: center;
 `
 export const DivImagem = styled.div`
+    width: 50vw;
 
+    > header{
+        margin: 88px 24px 0 25px;
+
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 22px;
+
+        color: #5C5C5C;
+    }
+
+    >div > img{
+        margin: 77px 0 155px 190px ;
+    }
 `
 export const DivDados = styled.div`
+    width: 50vw;
 
+    > header{
+        margin: 45px 212px 0 265px;
+
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 18px;
+        line-height: 22px;
+
+        color: #5C5C5C;
+    }    
 `
