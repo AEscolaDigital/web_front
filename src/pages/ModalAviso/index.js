7c2185716daeb407634b63ff307fd4924cadc761
Guilherme-Rodri <77001798+Guilherme-Rodri@users.noreply.github.com>
2021-09-29 10:56:07 -0300

    inserção de botões e texto, front finalizado

--- src/pages/ModalAviso/index.js
+++ src/pages/ModalAviso/index.js
@@ -1,7 +1,7 @@
-import { Container, DivHeader, DivImage } from "./styles";
+import { Container, DivHeader, DivImage, DivFooter } from "./styles";
 import Modal from "../../components/Modal";
 import aviso from "../../assets/modal_aviso/modal-aviso.svg"
-
+import BtnCancel from "../../components/BtnCancel"
 
 
 function ModalAviso() {
@@ -12,7 +12,7 @@ function ModalAviso() {
                 <DivHeader>
                     <div>
                         <h2>Parabéns Fulano, acaba de realizar com sucesso o seu cadastro,<br /> para a escola Abobrinha.</h2>
-                   
+
                         <h2>Aguarde agora a escola Abobrinha fazer a validação das<br /> informações apresentas, para seu login ser liberado.</h2>
                     </div>
                 </DivHeader>
@@ -21,8 +21,16 @@ function ModalAviso() {
                     <img src={aviso} />
                 </DivImage>
 
-                
+                <DivFooter>
+                    <div>
+                        <h2>Desejamos sucesso em sua jornada de aprendizagem</h2>
+                    </div>
 
+                    <div className="button">
+                        <BtnCancel text="Voltar ao login">
+                        </BtnCancel>
+                    </div>
+                </DivFooter>
 
             </Modal>
         </Container>
--- src/pages/ModalAviso/styles.js
+++ src/pages/ModalAviso/styles.js
@@ -4,7 +4,6 @@ import styled from "styled-components";
 export const Container = styled.div`
 
 `
-
 export const DivHeader = styled.div`
 
    >div>h2{
@@ -15,14 +14,24 @@ export const DivHeader = styled.div`
       padding-top:30px;
     }
 
-   >div:nth-child(2){
-
-   } 
-
 `
-
 export const DivImage = styled.div`
 
 padding-left:120px;
 
+`
+export const DivFooter = styled.div`
+
+>div>h2{
+
+     font-size:18px;
+      color: var(--color-text);
+      text-align:center;
+      margin-top:15px;
+}
+
+.button{
+  padding-top:40px;
+  padding-left:250px;
+}
 `
