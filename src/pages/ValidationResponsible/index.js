2b9a599e32935aa3c389130bed680ac70130cc74
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-09-29 10:57:07 -0300

    finalização da tela de validação do responsável

--- src/pages/ValidationResponsible/index.js
+++ src/pages/ValidationResponsible/index.js
@@ -1,29 +1,66 @@
-import { DivHeader, DivValidationLeft, } from "./styles";
+import { Container, DivHeader, DivValidationLeft, DivValidationRight, DivBtn, DivInputFile } from "./styles";
 import Modal from "../../components/Modal";
 import Input from "../../components/Input";
 import InputFile from "../../components/inputFile";
+import BtnCancel from "../../components/BtnCancel";
+import BtnSubmit from "../../components/BtnSubmit";
+import iconX from "../../assets/validationResponsible/iconX.svg"
 
 function ValidationResponsible() {
     return (
-        <Modal width="1100px" height="800px">
-            <DivHeader>
-                <h1>Validação do Responsável</h1>
+        <Modal width="1100px" height="850px">
+            <Container>
+            <img src={iconX} />
+                <DivHeader>
+                    <h1>Validação do Responsável</h1>
 
-                <img src=""/>
-            </DivHeader>
+                   
+                </DivHeader>
 
-            <DivValidationLeft>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
-                <Input label="Nome Completo"/>
+                <DivValidationLeft>
+                    <Input label="Nome Usuário" width="400px" />
+                    <Input label="Email Usuário" width="400px" />
+                    <Input label="Telefone Usuário" width="400px" />
+                    <Input label="Data de Nascimento" width="400px" />
+                    <Input label="Informe o RG" width="400px" />
+
+                    <DivInputFile>
+                        <InputFile />
+                    </DivInputFile>
+
+                </DivValidationLeft>
+
+
+
+                <DivValidationRight>
+                    <Input label="Informe o CPF" width="400px" />
+
+                <DivInputFile>
+                    <InputFile />
+                </DivInputFile>
+
+
+                    <Input label="Informe seu CEP" width="400px" />
+                    <Input label="Informe a rua" width="400px" />
 
                 <div>
-                    <InputFile/>
-                   
+                    <Input label="Informe o bairro" width="245px" />
+                    <Input label="Número" width="125px" />
                 </div>
-            </DivValidationLeft>
+
+                <DivInputFile>
+                    <InputFile />
+                </DivInputFile>
+
+
+                </DivValidationRight>
+
+
+            </Container>
+            <DivBtn>
+                <BtnCancel text="Inválido" />
+                <BtnSubmit text="Validar" />
+            </DivBtn>
         </Modal>
 
     );
--- src/pages/ValidationResponsible/styles.js
+++ src/pages/ValidationResponsible/styles.js
@@ -1,12 +1,65 @@
 import styled from "styled-components";
 
 
-export const DivHeader = styled.div`
+export const Container = styled.div`
     max-width: 1300px;
     margin: auto;
+
+    display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-template-areas: " Header header "  
+                            " DivValidationLeft DivValidationRight" ;
+
+    >img {
+        margin-left: 450px;
+        margin-top: 20px;
+    }
+`
+
+export const DivHeader = styled.div`
+   grid-area: Header;
+   
+    h1{
+        padding-top: 40px;
+        padding-bottom: 20px;
+        padding-left: 30px;
+    }   
     
+  
+`
+
+export const DivInputFile = styled.div`
+    padding-left: 20px;
+  
 `
 
 export const DivValidationLeft = styled.div`
-   
+  grid-area: DivValidationLeft;
+  margin-top: -20px;
+
+
 `
+
+export const DivValidationRight = styled.div`
+    grid-area: DivValidationRight;
+    
+    margin-top: -20px;  
+      
+    >div{
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        
+    }
+
+`
+
+export const DivBtn = styled.div`
+    display: grid;
+    grid-template-columns: 0.5fr 1fr;
+    
+    margin-left: 300px;
+    margin-top: 40px;
+    
+
+`
+
