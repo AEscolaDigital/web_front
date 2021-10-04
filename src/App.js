7973d5c1753d83d80387553e50ea0d9cf779336e
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-10-04 09:13:25 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,27 +3,14 @@ import Teams from "./pages/Teams";
 import Register from "./pages/Register";
 import Card from "./components/Card";
 import Validation from "./pages/Validation";
-import Input from "./components/Input";
-import InputFile from "./components/inputFile";
 
 function App() {
 
   return (
     <>
       <GlobalSyles />
-      <InputFile />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <div>
-
-        <InputFile />
-      </div>
+      <Teams />
+  
     </>
   );
 
