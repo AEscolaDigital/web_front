c758b84b2cce8a050ab37c874db95645a84c62e1
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-10-06 11:11:22 -0300

    arrumando o layout

--- src/pages/StudentRegistration/styles.js
+++ src/pages/StudentRegistration/styles.js
@@ -1,152 +1,80 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    max-width: 1300px;
+
+    max-width : 1300px;
     margin: auto;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    grid-template-areas:"header header" 
-                        "DivImage DivDados" ;
+    grid-template-areas: "header header"  
+                        "DivImage DivDados ";
     
 `
 
 export const DivHeader = styled.div`
-    max-width:1300px;
-    margin: auto;
-    grid-area:header;
     display: grid;
     grid-template-columns: 1fr 1fr;
-    margin-top:100px;
-  
-    >h1{
-        
-        color: var(--color-text);
-        font-size:22px;
-        font-weight:400;
-           
+    grid-area: header;
+
+    #divTextAluno{
+       margin-left: 300px;
     }
 
-    >h2{
+    h1{
         color: var(--color-text);
-        font-size:22px;
-        font-weight:200;
-        margin-left: 250px; 
+        font-size: 22px;
+        font-weight: 600;
+        padding-top: 80px;
+        margin-left: -70px;
+    }
+
+    h2{
+        color: var(--color-text);
+        font-size: 18px;
+        font-weight: 400;
+        padding-top:70px ;
+    }
+`
+
+export const DivImage = styled.div`
+    grid-area: DivImage;
+        img {
+            width: 800px;
+            margin-left: -100px;
+            margin-top: 100px;   
+        } 
+`
+
+export const DivDados = styled.form`
+    width: 500px;
+    grid-area: DivDados;
+    
+  
+    >div{
         display: grid;
         grid-template-columns: 1fr 1fr;
     }
 
-`
-{/*Seção Imagem */ }
-    export const DivImage = styled.div`
-        grid-area:DivImage;
-        align-items:left;
-        display:flex;
-        position:relative;
-        bottom:40px;
-        right:80px;
-    `
-{/************* */ }
-export const DivDados = styled.form`
-
-   
-    >h1{
-        color: var(--color-text);
-        font-size:22px;
-        font-weight:800;
-        justify-items:center;
-        position:relative;
-        bottom:80px;
+    div>div> p {
+        margin-top: 15px;
+        margin-bottom: 5px;
+        text-align: center;
+    }
         
+    #uploadResidencia{
+        align-items: center;
+        margin-left: 160px;
     }
-
-    >div:nth-child(2){
-        display:flex;
-       
-        >span{
-            margin:40px 0 0 15px;
-        }
-    }
-
-    >div:nth-child(3){
-        display:flex;
-       
-        >span{
-            margin:40px 0 0 15px;
-        }
-    }
-
-    >div:nth-child(4){
-        display:flex;
-       
-        >span{
-            margin:40px 0 0 15px;
-        }
-    }
- 
-    >div:nth-child(6){
-       display:flex;
-    }
-
-    >div:nth-child(7){
-       display:flex;
-    }
-
-    >p{
-        margin: 10px 0px 0 120px;
-        color: var(--color-text);
-        font-weight:600;
-    }
-
-    >div:nth-child(10){
-      margin: 5px 0 0 175px;
-    }
-
-    >div:nth-child(11){
-        
-        >p{
-           text-align:center;
-           color: var(--color-text);
-            margin-top:40px;
-            font-size:25px;
-        }
-
-        >div{
-            width:100px;
-            height:20px;
-            background:#5059C9;
-            margin-top:20px;
-            margin-bottom:20px;
-           margin-left:320px;
-        }
-        >div:nth-child(2){
-            width:100px;
-            height:20px;
-            background:white;
-            border:1px solid ;
-            float:left;
-           margin-left:180px;
-        }
-    }
-
-        >div.entrar{
-
-            float:right;
-            position:relative;
-            top:45px;
-            right:30px;
-        }
-
-        >div.cancelar{
-            margin-top:65px;
-            margin-bottom:50px;
-            position:relative;
-            left:75px;
-        }
-
-
 `
 
+export const DivBtn = styled.div`
+       display: grid;
+       grid-template-columns: 1fr 1fr;
+       padding: 20px;
+       margin-top: 25px;
+       
+       >div{
+           margin-left: 270px;
+       }
 
-
-
-
+`
