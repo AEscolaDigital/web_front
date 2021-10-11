f068325f3738be790cccc3712d42c4b738b8648b
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-10-11 11:23:12 -0300

    Excluído as páginas que não são de registro

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { GlobalSyles } from "./GlobalSytles";
-import Teams from "./pages/Teams";
 import Register from "./pages/Register";
-import RegisterPageStudent from "./pages/RegisterPageStudent";
-import Card from "./components/Card";
 
 function App() {
 
   return (
       <>
         <GlobalSyles/>
-        <RegisterPageStudent/>
+        <Register/>
       </>
   );
 
--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -53,4 +53,3 @@ export const DivImage = styled.div`
        justify-content: center;
        grid-area: DivImage;
        `
-
