76ccd2f48772386e838f8807b3102780e2204cc8
Emerson <77053593+Emerson916@users.noreply.github.com>
2021-10-17 21:53:19 -0300

    parte superior direita feita

--- src/pages/ListStudentsTask/index.js
+++ src/pages/ListStudentsTask/index.js
@@ -1,8 +1,8 @@
 import Nav from "../../components/Nav";
 import Header from "../../components/Header";
 import React from 'react';
-
-import { Container, ContainerTask } from "./styles";
+import imagePerfil from "../../assets/listStudentsTask/imagePerfil.svg";
+import { Container, ContainerTask, MateriaisReferencias, ContainerImage, ContainerTexts, ReferenciaAnexo } from "./styles";
 import NavTask from "../../components/NavTask";
 
 function Teams() {
@@ -11,11 +11,50 @@ function Teams() {
         <Container>
             <Header />
             <Nav />
-            <NavTask/>
+            <NavTask />
             <ContainerTask>
-                <div></div>
+
+                <ContainerImage>
+                    <img src={imagePerfil} />
+
+                    <p>Prof° Amanda </p>
+                    <p>14/07/2004</p>
+                </ContainerImage>
+
+                <ContainerTexts>
+                    <h2> Consumo de API ( CEP )</h2>
+                    <p>Data de entrega: 14/07/2004</p>
+                </ContainerTexts>
+
+
+                <MateriaisReferencias>
+                    <p>
+                        Consumir API de CEP
+                    </p>
+
+                    <div>
+                        <p>
+                            Viacep (https://youtu.be/imk6Y0viabg)
+                            Postmon (https://postmon.com.br/)
+                            CEP Aberto (https://cepaberto.com/)
+                        </p>
+                    </div>
+                    <p>
+                        Entregar o link do grithub, lembrando de habilitar o gitpage
+                    </p>
+
+                </MateriaisReferencias>
+
+                <ReferenciaAnexo>
+                    <h6>Materiáis de Referência</h6>
+                    <div>
+                        <p>batatinha frita 1,2,3... </p>
+                            
+                    </div>
+                </ReferenciaAnexo>
+
             </ContainerTask>
-             
+
         </Container>
 
     );
--- src/pages/ListStudentsTask/styles.js
+++ src/pages/ListStudentsTask/styles.js
@@ -4,21 +4,68 @@ export const Container = styled.div`
 `
 export const ContainerTask = styled.div`
 
-     width: 844px;
+     width: 800px;
      height: 80vw;
      
-     margin-top: 50px;
-     margin-left: 505px;
-  
-     background-color: beige;
+     margin-top: 60px;
+     margin-left: 520px;
+     border-radius: 20px;
+     background-color: #fff;
+     padding-top: 20px;   
+`
 
-     div{
-          width: 500px;
-          height: 50px;
+export const ContainerImage = styled.div`
+     height: 50px;
+     width: 50px;
+     border-radius:90px;
+     
+     margin-left: 30px;
+     
+     background: chartreuse;
+     gap: 30px;
+     display: flex;  
+`
 
-          background-color: red;
-          margin-left: 50px;
-     }
+export const ContainerTexts = styled.div`
+     width: 300px;
+     height: 50px;
+    
+     
+     margin-left: 110px;
+     
 `
 
 
+
+export const MateriaisReferencias = styled.div`
+     width: 300px;
+     height: 250px;
+     
+     margin-left: 110px;
+     margin-top: 20px;
+
+     div{
+          margin-top: 20px;
+          margin-bottom: 20px;
+          color: blue;
+     }
+`
+
+export const ReferenciaAnexo = styled.div`
+     width: 300px;
+     height: 100px;
+     
+     margin-left: 110px;
+
+     div{
+          width: 300px;
+          height: 30px;
+          
+          background: var(--color-background);
+          padding-top: 5px;
+          padding-left: 10px;
+
+          
+     }
+    
+`
