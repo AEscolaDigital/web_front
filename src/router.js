4542fa66d6b30188a3636edc62956ef3a25ad9cc
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-10-26 17:05:04 -0300

    Atualizações

--- src/router.js
+++ src/router.js
@@ -1,12 +1,8 @@
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { isSignedIn } from "./services/security";
 
-import ModalAviso from "./pages/ModalAviso";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Teams from "./pages/Teams";
-import RegisterResponsible from "./pages/RegisterResponsible";
-import StudentRegister from "./pages/StudentRegistration";
 
 function PrivateRoute({ children, ...rest }) {
     if (isSignedIn()) {
@@ -23,18 +19,6 @@ function Router() {
             <Switch>
                 <Route exact path="/" >
                     <Login />
-                </Route>
-                <Route exact path="/register">
-                    <Register />
-                </Route>
-                <Route exact path="/register/student">
-                    <StudentRegister />
-                </Route>
-                <Route exact path="/register/responsible">
-                    <RegisterResponsible />
-                </Route>
-                <Route exact path="/register/modalaviso">
-                    <ModalAviso />
                 </Route>
                 <PrivateRoute path="/teams">
                     <Teams />
