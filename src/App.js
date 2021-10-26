1fb2afb348b2ef2d971e6330528139023147d125
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-10-26 17:10:39 -0300

    Exclusão de pages, que não corresponde a essa branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import { GlobalSyles } from "./GlobalSytles";
-import Teams from "./pages/Teams";
-import Register from "./pages/Register";
-import Card from "./components/Card";
-import Login from "./pages/Login";
 import MemberRegister from "./pages/memberRegistration";
 
 function App() {
