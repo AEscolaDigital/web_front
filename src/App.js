e9113c1c54e763ad29b1cb16620c80e3f33f311b
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-03 10:25:55 -0300

    Retirando a page que não são dessa branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import { GlobalSyles } from "./GlobalSytles";
-import Teams from "./pages/Teams";
-import Register from "./pages/Register";
-import Card from "./components/Card";
-import Login from "./pages/Login";
 import MemberClassCreate from "./pages/MemberClassCreate";
 
 function App() {
