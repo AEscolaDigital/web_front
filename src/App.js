a91c2d91c8fa1ffe4d7b2afd716d48c38153c260
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-11-17 16:26:51 -0300

    Merge da develop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { GlobalSyles } from "./GlobalSytles";
+import Router from "./router";
+
+function App() {
+  return (
+      <>
+        <GlobalSyles/>
+        <Router/>
+      </>
+  );
+
+}
+
+export default App;
